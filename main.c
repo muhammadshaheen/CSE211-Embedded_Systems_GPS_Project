c2ec2b40948f391259fe7318ebe47f9f202d1584
AdhamIbrahim74 <85297947+AdhamIbrahim74@users.noreply.github.com>
2021-06-06 22:25:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,26 @@
+#include "tm4c123gh6pm.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include "tm4c123.h"
+
+#define pi  3.14159265358979323846
+
+void delayMs(int times);
+void delayUs(int times2);
+void LCD_command(unsigned char command);
+void LCD_data(unsigned char data);
+void LCD_init(void);
+void print_string(char* x);
+void Red_Led_On(unsigned char out);
+float Get_Distance(float long1, float lat1, float long2, float lat2);
+void Init_PortF(void);
+bool Reach_Destination(float d);
+
+float distance_test;        //Global Variable For Distance
+
 int main(void){
     SCB->CPACR |= ((3UL << 10*2) | (3UL << 11*2) );                       // to enable float datatype
     Init_PortF();                                                         // to initials Port F 
